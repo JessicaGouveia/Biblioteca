5e72ddb1582728f771712eacd48aa9ff45d7acb9
Jéssica Gouveia <jessicacostagouveia23@gmail.com>
2018-11-11 19:46:59 -0200

    Criação das models
    Todas as models criadas

---
+++ src/Models/ColecaoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemTeca.Models
+{
+    public class ColecaoModel
+    {
+        public int IdColecao { get; set; }
+        public string NomeColecao { get; set; }
+        public int QuantidadeColecao { get; set; }
+        public string VolumeColecao { get; set; }
+        public bool AtivoColecao { get; set; }
+        public bool DeletadoColecao { get; set; }
+        public DateTime DataCriacao { get; set; }
+
+    }
+}
---
+++ src/Models/LogSistemaModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemTeca.Models
+{
+    public class LogSistemaModel
+    {
+        public int IdLog { get; set; }
+        public DateTime DataOperacao { get; set; }
+        public string UsuarioOperacao { get; set; }
+        public string Descricao { get; set; }
+    }
+}
