53135042f185198981003256e1bc2a84edc5d5eb
Jéssica Gouveia <jessicacostagouveia23@gmail.com>
2018-11-17 15:54:46 -0200

    Alterar Categoria pronto
    Categoria está completa

--- src/Cadastros/Alterar/FmrAlterarCategoria.cs
+++ src/Cadastros/Alterar/FmrAlterarCategoria.cs
@@ -1,25 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using SystemTeca.Controllers;
+using SystemTeca.Models;
 
 namespace SystemTeca.Cadastros.Alterar
 {
     public partial class FmrAlterarCategoria : Form
     {
-        public FmrAlterarCategoria()
+        private CategoriaController categoriaController;
+        private CategoriaModel categoria;
+
+        public FmrAlterarCategoria(int idCategoria)
         {
             InitializeComponent();
+            categoriaController = new CategoriaController();
+            categoria = categoriaController.ConsultaPorId(idCategoria);
+
+            Nome_txt.Text = categoria.NomeCategoria;
+            comboBox1.SelectedIndex = comboBox1.FindStringExact(((categoria.AtivoCategoria.Value)? "Sim":"Não"));
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            this.Dispose();
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Nome_txt.Text != "" && categoria.AtivoCategoria != null)
+            {
+                categoria.NomeCategoria = Nome_txt.Text;
+                var dbResultado = categoriaController.Atualizar(categoria);
+                MessageBox.Show(dbResultado.Mensagem);
+                if (dbResultado.NumRetorno > 0)
+                {
+                    this.Close();
+                }
+
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "Sim":
+                    categoria.AtivoCategoria = true;
+                    break;
+                default:
+                    categoria.AtivoCategoria = false;
+                    break;
+            }
         }
     }
 }
