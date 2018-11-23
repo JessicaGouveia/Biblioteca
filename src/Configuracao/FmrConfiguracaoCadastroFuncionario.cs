6db405e51e3e434028e4aaf8018ebe1283c19008
Daniel Polo <jessicacostagouveia23@gmail.com>
2018-11-23 19:47:09 -0200

    Cadastro de Usuario , Cadastro de Funcionario

--- src/Configuracao/FmrConfiguracaoCadastroFuncionario.cs
+++ src/Configuracao/FmrConfiguracaoCadastroFuncionario.cs
@@ -7,14 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SystemTeca.Controllers;
+using SystemTeca.Data;
+using SystemTeca.Models;
 
 namespace SystemTeca.Configuracao
 {
     public partial class FmrConfiguracaoCadastroFuncionario : Form
     {
+
+        private PessoaController pessoaController;
+        private PessoaModel pessoa;
+
         public FmrConfiguracaoCadastroFuncionario()
         {
             InitializeComponent();
+            pessoaController = new PessoaController();
+            pessoa = new PessoaModel();
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -24,7 +33,38 @@ namespace SystemTeca.Configuracao
 
         private void button4_Click(object sender, EventArgs e)
         {
+            if (NomeTxT.Text != "" &&
+                TelefoneTxT.Text != "" &&
+                RGTxT.Text != "" &&
+                CPFTxT.Text != "" &&
+                EnderecoTxT.Text != "" &&
+                NumeroTxT.Text != "" &&
+                BairroTxT.Text != "" &&
+                CidadeTxT.Text != "" &&
+                UFTxT.Text != "")
+            {
 
+                // Passa os dado para a Model os Dados coletados pelo usuario
+                pessoa.NomePessoa = NomeTxT.Text;
+                pessoa.NumeroTelefone = TelefoneTxT.Text;
+                pessoa.Rg = RGTxT.Text;
+                pessoa.Cpf = CPFTxT.Text;
+                pessoa.Cep = CEPTxT.Text;
+                pessoa.Logradouro = EnderecoTxT.Text;
+                pessoa.Numero = NumeroTxT.Text;
+                pessoa.Bairro = BairroTxT.Text;
+                pessoa.Cidade = CidadeTxT.Text;
+                pessoa.Uf = UFTxT.Text;
+                pessoa.AtivoPessoa = true;
+
+                var dbResultado = pessoaController.Inserir(pessoa);
+
+                MessageBox.Show(dbResultado.Mensagem);
+                if (dbResultado.NumRetorno > 0)
+                {
+                    this.Close();
+                }
+            }
         }
     }
 }
